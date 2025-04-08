0bf59a41a673d9f84f85c03e7b2cf49e4d977fe7
AKMOUNE Feriel <feriel.akmoune@etu.usthb.dz>
2025-04-08 18:59:46 +0100

    Delete ecommerce directory

--- ecommerce/cart/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product, CartItem, Order, OrderItem  # Ajoute tes modèles ici
-
-# Register your models here.
-
-admin.site.register(Product)
-admin.site.register(CartItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)
